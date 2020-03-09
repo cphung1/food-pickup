4feb69a32687d3adb29957200384222321fcb670
cphung1 <cphung97@gmail.com>
2020-03-09 03:59:03 +0000

    fied quantity incrementor

--- public/scripts/quantity_incrementor.js
+++ public/scripts/quantity_incrementor.js
@@ -1,25 +1,39 @@
 $(function() {
-  function incrementVal(selector) {
-    let $item = selector;
-    let currentVal = $item.attr("value");
-    $item.attr("value", parseInt(currentVal) + 1 );
-  }
+  // function incrementVal(selector) {
+  //   let $item =  selector;
+  //   console.log($item)
+  //   let currentVal = $item.attr("value");
+  //   $item.attr("value", parseInt(currentVal) + 1 );
+  // }
 
-  function decreaseVal(selector) {
-    let $item = selector;
-    let currentVal = $item.attr("value");
-    if (currentVal > 1) {
-      $item.attr("value", parseInt(currentVal) - 1 );
-    }
-  }
+  // function decreaseVal(selector) {
+  //   let $item = selector;
+  //   let currentVal = $item.attr("value");
+  //   if (currentVal > 1) {
+  //     $item.attr("value", parseInt(currentVal) - 1 );
+  //   }
+  // }
 
-  $('#increase').click(function() {
-    incrementVal($('#quantity').atrr("class", "<%= item.id %>"));
+  // $('#increase').click(function() {
+  //   console.log($('#quantity'));
+  //   incrementVal($('#quantity'));
+  // });
+
+  // $('#decrease').click(function() {
+  //   decreaseVal($('quantity').find('input').attr('id'));
+  // });
+
+  $(".increase").click(function() {
+    updateValue(this, 1);
+  });
+  $(".decrease").click(function() {
+      updateValue(this, -1);
   });
 
-  $('#decrease').click(function() {
-    decreaseVal($('#quantity').atrr("class", "<%= item.id %>"));
-  });
-
+  function updateValue(obj, delta) {
+      let item = $(obj).parent().find("input");
+      let newValue = parseInt(item.val(), 10) + delta;
+      item.val(Math.max(newValue, 1));
+  }
 
 });
