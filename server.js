b6c109b88f9ad4d99cf68fc6742ecd3e100a829b
cphung1 <cphung97@gmail.com>
2020-03-09 06:03:03 +0000

    created /confirmed page for customers

--- server.js
+++ server.js
@@ -35,13 +35,15 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const ordersConfirmed = require("./routes/ordersConfirmed");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
-
+// itemRouter = require('./routes/itemsRoute');
+app.use(ordersConfirmed);
 
 
 const { browse } = require('./db/queries');
