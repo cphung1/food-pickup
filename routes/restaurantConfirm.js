371fbb1f84f936621d310c83fddf5ab1367df07a
cphung1 <cphung97@gmail.com>
2020-03-10 00:28:58 +0000

    added table to rest confirm page && estimate time button css

--- routes/restaurantConfirm.js
+++ routes/restaurantConfirm.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router  = express.Router();
+const { checkoutItems } = require('../db/queries');
 
 router.get('/restaurant_confirm', (req, res) => {
-  res.render('restaurant');
+  checkoutItems((err, checkoutStuff) => {
+    if (err) {
+      return res.render('error', { err });
+    }
+    res.render('restaurant', { checkoutStuff });
+  });
 });
 
 module.exports = router;
