515760298e0b34e6f0c56cddab984b09fa8d0c98
Alex <alextheprogrammer21@gmail.com>
2020-03-10 18:35:20 +0000

    Added cookies to login feature

--- public/scripts/menuScripts.js
+++ public/scripts/menuScripts.js
@@ -1,11 +1,20 @@
 //----------------------------------------JQUERY FUNCTIONS--------------------------------//
 $( document ).ready(function() {
 
+  let is_logged_in = document.cookie;
+
+  console.log(is_logged_in);
+  if (!is_logged_in) {
   $( ".login_button" ).click(function( event ) {
     event.preventDefault();
     $("login_form").css("display", "none");
     $('top_bar').append(`<p>Logged in as Cathy</p>`);
     $('top_bar').css("font-weight", "bold");
-  });
-
+    document.cookie = "is_logged_in=true";
+  })
+} else {
+  $("login_form").css("display", "none");
+    $('top_bar').append(`<p>Logged in as Cathy</p>`);
+    $('top_bar').css("font-weight", "bold");
+}
 });
