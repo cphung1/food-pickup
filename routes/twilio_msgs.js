4149f3e5b4fd14bb9d9782e3128c1321aa095266
cphung1 <cphung97@gmail.com>
2020-03-26 05:38:46 +0000

    added screenshots, omit phone num

--- routes/twilio_msgs.js
+++ routes/twilio_msgs.js
@@ -7,8 +7,8 @@ const orderConfirmed = function (order_id) {
   twilioClient.messages
     .create({
       body: `http://localhost:8080/restaurant_confirm/${order_id}`,
-      from: '+16042621059',
-      to: '+17809918297'
+      from: TWILIO_NUM,
+      to: YOUR_NUM
     })
     .then(message => console.log(message.sid));
 }
@@ -17,8 +17,8 @@ const timeConfirmed = function (time_est) {
   twilioClient.messages
   .create({
     body: `Your order has been confirmed. It will be ready in approximately ${time_est} minutes.`,
-    from: '+16042621059',
-    to: '+16474703858'
+    from: TWILIO_NUM,
+    to: YOUR_NUM
   })
   .then(message => console.log(message.sid));
 }
@@ -27,8 +27,8 @@ const orderCompleted = function () {
   twilioClient.messages
   .create({
     body: `Your order is completed! 😊`,
-    from: '+16042621059',
-    to: '+16474703858'
+    from: TWILIO_NUM,
+    to: YOUR_NUM
   })
   .then(message => console.log(message.sid));
 }
